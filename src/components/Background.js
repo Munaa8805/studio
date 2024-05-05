8e1b4bbb50b2394819e72e379609f472225d059a
Munaa8805 <munaa.tsetsegmaa@gmail.com>
2024-05-05 09:57:14 -0700

    initial

---
+++ src/components/Background.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Background = () => {
+  return (
+    <div id="hero" className="h-[40vh] w-full absolute top-0 right-0 -z-10">
+      <div className="container mx-auto">
+        <div className="max-w-lg mt-32 mb-32 p-4 font-sans text-2xl text-white uppercase border-2 md:p-10 md:m-32 md:mx-0 md:text-6xl">
+          Impressive Experiences That Deliver
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Background;
